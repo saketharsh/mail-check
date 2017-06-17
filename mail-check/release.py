cfe5665ae99074b5690db99858737984a3f6d89d
saketharsh <sharsh2010@gmail.com>
2017-06-17 21:46:52 +0530

    Initiated creating the cli

---
+++ mail-check/release.py
@@ -0,0 +1,15 @@
+"""Release file for mail-check"""
+
+name = 'mail-check'
+major = '0'
+minor = '1'
+
+__version__ = major + '.' + minor
+
+__date__ = '2017-12-18'  ## YYYY-MM-DD
+
+__description__ = 'Terminal based mail-reader'
+
+__author__ = 'Saket Harsh'
+__email__ = 'sharsh@iitk.ac.in'
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools import setup
+
+if sys.argv[-1] == 'setup.py':
+    print('To install, run \'python setup.py install\'')
+    print()
+
+sys.path.insert(0, 'mail-check')
+import release
+
+if __name__ == "__main__":
+    setup(
+        name = release.name,
+        version = release.__version__,
+        author = release.__author__,
+        author_email = release.__email__,
+        description = release.__description__,
+        url='https://github.com/saketharsh/mail-check',
+        keywords='python based terminal mail-checker',
+        packages = ['mail-check',
+                    'ping_me.data',
+                    'ping_me.utils'],
+        license = 'MIT License',
+        entry_points = {
+            'console_scripts': [
+            'ping-me = ping_me.ping:main',
+            'get-ping = ping_me.GET:main'
+            ]
+        },
+        install_requires = ['phonenumbers', 'requests', 'pycrypto',
+                            'python-dateutil', 'parsedatetime'],
+        test_suite = 'nose.collector',
+        tests_require = ['nose>=0.10.1']
+    )
